e657378e1425361350eccc66adee0c916aa697e2
wangbo <wangbo@bluemoon.com.cn>
2016-08-05 16:31:37 +0800

    first commit

---
+++ BWSwiftResearch-MacOS/BWSwiftResearch-MacOS/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BWSwiftResearch-MacOS
+//
+//  Created by BobWong on 16/8/5.
+//  Copyright © 2016年 BobWong. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
